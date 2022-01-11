d5070cc94e3fe9f283bb2495810bb5ab21f618d0
Ebo Onwunali <ebo@njgeneral.com>
2022-01-11 01:01:37 -0500

    Added methods representing Arithmetic arguments

--- CSharpMethodsA/CSharpMethodsA/Program.cs
+++ CSharpMethodsA/CSharpMethodsA/Program.cs
@@ -4,6 +4,7 @@ namespace CSharpMethodsA // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
             string yourName;
@@ -36,6 +37,51 @@ namespace CSharpMethodsA // Note: actual namespace depends on the project name.
                 "However, N.A.S ... Nas, Nas, Nas, is the best to do it! My GUY! I knew you were super clever... " +
                 "{4} is my all-time favorite too and G.O.A.T POTUS, hands down!", yourName, favoriteColor, favoriteAnimal, favoriteBand, favoritePresident);
 
+            var result1 = Multiply(9,5);
+            Console.WriteLine(result1);
+
+            var result2 = Divide(222,22);
+            Console.WriteLine(result2);
+
+            var result3 = Modulus(56, 9);
+            Console.WriteLine(result3);
+
+            var result4 = Sum(4, 3);
+            Console.WriteLine(result4);
+
+            var result5 = Subtract(19, 7);
+            Console.WriteLine(result5);
+
         }
+
+        public static int Multiply(int num1, int num2)
+        {
+            var mult = num1 * num2;
+            return mult;
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            var div = num1 / num2;
+            return div;
+        }
+
+        public static int Modulus(int num1, int num2)
+        {
+            var mod = num1 % num2;
+            return mod;
+        }
+        public static int Sum(int num1, int num2)
+        {
+            var sum = num1 + num2;
+            return sum;
+        }
+
+        public static int Subtract(int num1, int num2)
+        {
+            var sub = num1 - num2;
+            return sub;
+        }
+
     }
 }
